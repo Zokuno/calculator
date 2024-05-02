d14a7de45385fb2f1f3427d5b431aaa1baf46eb2
Joel <joel.brookes@gmail.com>
2024-05-03 00:28:19 +1000

    Added parseFloat to try to get addition to work, so far unsuccessful

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let divideBtn = document.querySelector("#divide-btn");
 let multiplyBtn = document.querySelector("#multiply-btn");
 let subtractBtn = document.querySelector("#subtract-btn");
 let addBtn = document.querySelector("#add-btn");
-let equalsBtn = document.querySelector("equals-btn");
+let equalsBtn = document.querySelector("#equals-btn");
 let zeroBtn = document.querySelector("#zero-btn");
 let oneBtn = document.querySelector("#one-btn");
 let twoBtn = document.querySelector("#two-btn");
@@ -24,6 +24,9 @@ let nineBtn = document.querySelector("#nine-btn");
 let decimalBtn = document.querySelector("#decimal-btn");
 
 calcScreen1.innerHTML = "";
+let nmbr1 = 0;
+let nmbr2 = 1;
+let oper = "";
 
 // Event Listener List
 clearBtn.addEventListener("click", () => {
@@ -32,16 +35,14 @@ clearBtn.addEventListener("click", () => {
 });
 
 zeroBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
-    
+        // Left blank to stop "0" being entered multiple times after using Clear
     } else {
         calcScreen2.innerHTML = calcScreen2.innerHTML + "0";
     }
 });
 
 oneBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "1";
@@ -51,7 +52,6 @@ oneBtn.addEventListener("click", () => {
 });
 
 twoBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "2";
@@ -61,7 +61,6 @@ twoBtn.addEventListener("click", () => {
 });
 
 threeBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "3";
@@ -71,7 +70,6 @@ threeBtn.addEventListener("click", () => {
 });
 
 fourBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "4";
@@ -81,7 +79,6 @@ fourBtn.addEventListener("click", () => {
 });
 
 fiveBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "5";
@@ -91,7 +88,6 @@ fiveBtn.addEventListener("click", () => {
 });
 
 sixBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "6";
@@ -101,7 +97,6 @@ sixBtn.addEventListener("click", () => {
 });
 
 sevenBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "7";
@@ -111,7 +106,6 @@ sevenBtn.addEventListener("click", () => {
 });
 
 eightBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "8";
@@ -121,7 +115,6 @@ eightBtn.addEventListener("click", () => {
 });
 
 nineBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML === "0") {
         calcScreen2.innerHTML = "";
         calcScreen2.innerHTML = "9";
@@ -131,14 +124,27 @@ nineBtn.addEventListener("click", () => {
 });
 
 decimalBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen2.innerHTML;
     if (calcScreen2.innerHTML.includes(".")) {
-        
+        // Left blank to not include another "." if one already exists
     } else {
         calcScreen2.innerHTML = calcScreen2.innerHTML + ".";
     }
 });
 
 addBtn.addEventListener("click", () => {
-    calcScreen1.innerHTML = calcScreen1.innerHTML + "+";
+    if (calcScreen2.innerHTML === "0") {
+        // Left blank to stop "0" being entered to calcscreen1 after using a function
+    } else {
+        nmbr1 = parseFloat(calcScreen2.innerHTML);
+        oper = "+";
+        calcScreen1.innerHTML = calcScreen1.innerHTML + calcScreen2.innerHTML;
+        calcScreen1.innerHTML = calcScreen1.innerHTML + " + ";
+    
+        // calcScreen2.innerHTML = nmbr1 + nmbr2;
+    }
+});
+
+equalsBtn.addEventListener("click", () => {
+    let result = nmbr1 + oper + nmbr2;
+    calcScreen2.innerHTML = parseFloat(result);
 });
